987f758637099593249265c261f39f02b98a9f94
Robson Santos <allrobson99@gmail.com>
2024-04-14 19:49:12 -0300

    arrumando comentários

--- predict_model.py
+++ predict_model.py
@@ -1,21 +1,20 @@
 import pandas as pd
 import mlflow
 
-# Carregar o modelo treinado
 model = mlflow.sklearn.load_model(r'C:\Users\allis\Downloads\MDCC\Untitled Folder\mlruns\0\e7142990b00c4716b9f71fbcf48362c6\artifacts\best_model')
 
-# Carregar os dados de teste
+# Carregando os dados de teste
 X_test = pd.read_csv('transformed_X_test.csv')
-y_test = pd.read_csv('transformed_y_test.csv')  # Substitua 'column_name' pelo nome da coluna alvo
+y_test = pd.read_csv('transformed_y_test.csv')
 
-# Fazer previsões
+# Fazendo as  previsões
 predictions = model.predict(X_test)
 
-# Avaliar as previsões
+# Avaliando as previsões
 test_score = model.score(X_test, y_test)
 print("Test Score:", test_score)
 
-# Salvar o resultado das previsões como artefato
+# Salvando o resultado das previsões como artefato
 pd.DataFrame(predictions).to_csv('predictions.csv', index=False)
 mlflow.log_artifact('predictions.csv')
 
